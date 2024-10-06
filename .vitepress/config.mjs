bce0b4a1e1a287159006808b094285ac3273d0df
XingJi-love <3968523679@qq.com>
2024-10-06 18:41:56 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/xuevitepress/',
+  base: '/vitepress/',
   title: "宝小宝",
   description: "xiaobao",
   head: [["link", { rel: "icon", href: "/xuevitepress/ico.ico" }]],
